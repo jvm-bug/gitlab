e61c62af05658f92a488a8042f7c10749c7e905f
zhuozhixiong <good509@qq.com>
2023-04-09 19:40:37 +0800

    第一个版本

--- untitled/src/com/zhuo/Demo01.java
+++ untitled/src/com/zhuo/Demo01.java
@@ -4,6 +4,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("helloword");
         System.out.println("这是第一个目");
-        System.out.println("zhogguo");
+
     }
 }
